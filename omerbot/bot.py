481d4d89fd0f2c6aa01453e1d2f07b0a89bf687a
G Rosen <gillian.t.rosen@gmail.com>
2023-04-24 21:01:03 -0400

    broke aspect string creation into its own function, started test skeleton

--- omerbot/bot.py
+++ omerbot/bot.py
@@ -61,21 +61,9 @@ class Bot(discord.Client):
 #        posts = [text[index: index + 2000] for index in range(0, len(text), 2000)]
     #    inner_idx = 5
     #    outer_idx = 6
-        idxs = [inner_idx,outer_idx]
-        print(idxs)
-        aspect_strs = ["",""]
-        for x in range(2):
-            curr_idx = idxs[x]
-            
-            heart_string = colors[curr_idx] 
-            if heart_string == "red":
-                heart_string = ":heart:"
-            else:
-                heart_string = f":{heart_string}_heart:"
-            curr_str = f"{heart_string}{tl_aspects[curr_idx]}{heart_string}"
-            aspect_strs[x] = curr_str            
- 
-### STRING CONSTRUCTION ###
+
+
+  ### STRING CONSTRUCTION ###
      
         greg_string = greg_today.strftime("%A, %B %d")
         day_string = "days"
@@ -107,21 +95,19 @@ class Bot(discord.Client):
         if holiday_tonight: 
             eng_extra = f"It is also {holiday_today}."
 
-        eng_intro = f"Today is {greg_string}. After sundown, count the Omer:" 
+        eng_intro = f"Today is {greg_string}. After sundown, count the Omer!" 
         eng_oc_string = f"Today is {omer_day} {day_string}, which are {week_string}{and_string}{weekday_string} of the Omer:"
-        eng_aspect_string = f"{aspect_strs[0]} within {aspect_strs[1]}" 
-
-        heb_general_blessing = f""
-        tl_general_blessing = f""
-        eng_general_blessing = f""
-        
+        eng_aspect_string = self.construct_aspect_strings(inner_idx,outer_idx)
+         
         heb_todays_blessing = f""
         tl_todays_blessing = f""    
         eng_todays_blessing = f"" 
  	
-        full_intro =  f"{eng_intro}\n\n**{eng_oc_string}\n{eng_aspect_string}.**"  
-        if eng_extra: full_intro += f"\nIt's also {eng_extra}." 
-        post = full_intro
+        eng_count = f"**{eng_oc_string}\n{eng_aspect_string}.**"  
+        if eng_extra: eng_count += f"\nIt's also {eng_extra}." 
+        general_blessing = f"*if you've counted every day:*\n{self.data['heb_blessing']}\n{self.data['tl_blessing']}\n{self.data['eng_blessing']}"
+
+        post = f"{eng_intro}\n\n{general_blessing}\n\n{eng_count}"
         try:
             # Post.
             await channel.send(post)
@@ -132,6 +118,56 @@ class Bot(discord.Client):
         except Exception as e:
             self.log(str(e))
 
+
+    def construct_aspect_strings(self,inner_idx: int,outer_idx: int):
+        """ 
+        construct the strings for the aspects given.
+    
+        param inner_idx: inner aspect index
+        param outer_idx: outer aspect index
+        """ 
+        heb_aspects = self.data["heb_aspects"]
+        tl_aspects = self.data["tl_aspects"]
+        eng_aspects = self.data["eng_aspects"] 
+        colors = self.data["colors"]
+        
+        idxs = [inner_idx,outer_idx]
+        heb_aspect_strs = ["",""]
+        tl_aspect_strs = ["",""]
+        eng_aspect_strs = ["",""]
+     
+        for x in range(2):
+            curr_idx = idxs[x]
+            # hebrew & transliteration
+            heb_aspect_strs[x] = f"{heb_aspects[curr_idx]}"            
+            tl_aspect_strs[x] = f"{tl_aspects[curr_idx]}"
+            # english 
+            heart_string = colors[curr_idx] 
+            if heart_string == "red":
+                heart_string = ":heart:"
+            else:
+                heart_string = f":{heart_string}_heart:"
+            eng_aspect_strs[x] = f"{heart_string} {tl_aspects[curr_idx]} ({eng_aspects[curr_idx]}) {heart_string}"
+                       
+    
+        heb_join = "\u05e9\u05c1\u05b6\u05d1\u05bc\u05b0"
+        tl_join = "sheb'"
+        if outer_idx == 1: # gevurah uses a different connector word
+            heb_join = "\u05e9\u05c1\u05b6\u05d1\u05bc\u05b4"
+            tl_join = "shebi"
+        
+        heb_aspect_string = f"{heb_aspect_strs[0]} {heb_join}{heb_aspect_strs[1]}" 
+        tl_aspect_string = f"{tl_aspect_strs[0]} {tl_join}{tl_aspect_strs[1]}"
+        eng_aspect_string = f"{eng_aspect_strs[0]} within {eng_aspect_strs[1]}" 
+        return eng_aspect_string
+
+    def test(self) -> bool:
+        """
+        test function
+        """ 
+        test_pass = true
+        return test_pass
+     
     def log(self, message: str) -> None:
         """
         Log a message.
